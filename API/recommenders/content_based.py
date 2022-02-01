ebf5928bbe130b36a85510ade3d540d1dee40a62
Karabo <kmampura@student.wethinkcode.co.za>
2022-02-01 03:58:01 +0200

    managed to get a different output in the predictions

--- API/recommenders/content_based.py
+++ API/recommenders/content_based.py
@@ -35,11 +35,13 @@ from sklearn.metrics.pairwise import cosine_similarity
 from sklearn.feature_extraction.text import CountVectorizer
 
 # Importing data
+#/home/explore-student/unsupervised_data/unsupervised_movie_data/
 movies = pd.read_csv('resources/data/movies.csv', sep = ',')
-ratings = pd.read_csv('resources/data/ratings.csv')
+ratings = pd.read_csv('resources/data/old/ratings.csv')
+movies.dropna(inplace=True)
+
 movies_df = pd.read_csv('/home/explore-student/unsupervised_data/unsupervised_movie_data/movies.csv')
 imdb_data = pd.read_csv('/home/explore-student/unsupervised_data/unsupervised_movie_data/imdb_data.csv')
-movies.dropna(inplace=True)
 
 def data_preprocessing(subset_size):
     """Prepare data for use within Content filtering algorithm.
@@ -55,8 +57,8 @@ def data_preprocessing(subset_size):
         Subset of movies selected for content-based filtering.
 
     """
-     ###my additions
-    movies = movies_df
+    
+    ###my additions
     df = imdb_data[['movieId','title_cast','director', 'plot_keywords']]
     df = df.merge(movies[['movieId', 'genres', 'title']], on='movieId', how='inner')
     df['year'] = df['title'].str.extract(r"\((\d+)\)", expand=False)
@@ -86,9 +88,9 @@ def data_preprocessing(subset_size):
     ##################diff cell 3 
     df['corpus'] = ''
     corpus = []
-
+    
     # List of the columns we want to use to create our corpus 
-    columns = ['plot_keywords', 'genres', 'year']
+    columns = [ 'plot_keywords']
 
     # For each movie, combine the contents of the selected columns to form it's unique corpus 
     for i in range(0, len(df['movieId'])):
@@ -106,45 +108,43 @@ def data_preprocessing(subset_size):
     ##################diff cell 3
     ###my additions
     
-   
-    #####my additions
     # Split genre data into individual words.
-    # movies = movies_df
-    # movies['keyWords'] = movies['genres'].str.replace('|', ' ')
+    movies['keyWords'] = movies['genres'].str.replace('|', ' ')
+    print(movies.head(3))
     # Subset of the data
     movies_subset = movies[:subset_size]
     
+    #return movies_subset
     the_subset = df[:subset_size]
-    return the_subset
+    #print(the_subset.head(5))
+    #print("rrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrr")
+    return movies_subset
 
 # !! DO NOT CHANGE THIS FUNCTION SIGNATURE !!
 # You are, however, encouraged to change its content.  
 def content_model(movie_list,top_n=10):
     """Performs Content filtering based upon a list of movies supplied
        by the app user.
-
     Parameters
     ----------
     movie_list : list (str)
         Favorite movies chosen by the app user.
     top_n : type
         Number of top recommendations to return to the user.
-
     Returns
     -------
     list (str)
         Titles of the top-n movie recommendations to the user.
-
     """
     # Initializing the empty list of recommended movies
     recommended_movies = []
-    data = data_preprocessing(200)
+    data = data_preprocessing(27000)
     # Instantiating and generating the count matrix
     count_vec = CountVectorizer()
-    # count_matrix = count_vec.fit_transform(data['keyWords'])
     count_matrix = count_vec.fit_transform(data['corpus'])
     indices = pd.Series(data['title'])
     cosine_sim = cosine_similarity(count_matrix, count_matrix)
+    #print("33333333333333333333333333333333333333333333")
     # Getting the index of the movie that matches the title
     idx_1 = indices[indices == movie_list[0]].index[0]
     idx_2 = indices[indices == movie_list[1]].index[0]
@@ -158,14 +158,17 @@ def content_model(movie_list,top_n=10):
     score_series_2 = pd.Series(rank_2).sort_values(ascending = False)
     score_series_3 = pd.Series(rank_3).sort_values(ascending = False)
     # Getting the indexes of the 10 most similar movies
+    #print("2222222222222222222222222222222222222222")
     listings = score_series_1.append(score_series_2).append(score_series_3).sort_values(ascending = False)
 
     # Store movie names
     recommended_movies = []
     # Appending the names of movies
-    top_50_indexes = list(listings.iloc[1:top_n*2].index)
+    top_50_indexes = list(listings.iloc[1:50].index)
     # Removing chosen movies
     top_indexes = np.setdiff1d(top_50_indexes,[idx_1,idx_2,idx_3])
     for i in top_indexes[:top_n]:
         recommended_movies.append(list(movies['title'])[i])
+    
+    #print("111111111111111111111111111111111")
     return recommended_movies
